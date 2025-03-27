eb94f72209f20a7e8b6a4b208a3d3e7403390fa4
rockruler73 <casey.t.rock@gmail.com>
2025-03-27 18:17:33 -0400

    removed credentials from var and main

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,18 +4,6 @@ variable "aws_region" {
   default     = "us-west-2"
 }
 
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
-
 variable "ami_id" {
   description = "AMI ID for EC2"
   type        = string
