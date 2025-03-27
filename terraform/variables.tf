42d4e442b4da38878923fc0b351404123d453360
rockruler73 <casey.t.rock@gmail.com>
2025-03-27 14:52:49 -0400

    edited variables.tf to use different ami

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,7 +17,7 @@ variable "aws_secret_key" {
 variable "ami_id" {
   description = "The AMI ID to use for the EC2 instance"
   type        = string
-  default     = "ami-075686beab831bb7f"
+  default     = "ami-0f9d441b5d66d5f31"
 }
 
 variable "docker_image" {
